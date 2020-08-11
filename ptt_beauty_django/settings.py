a8fc32bb3a652a161d3b829bbcb89d643303ce78
ben_wang <uncleben006@gmail.com>
2020-08-11 20:05:44 +0800

    Update heroku config

--- ptt_beauty_django/settings.py
+++ ptt_beauty_django/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ptt-beauty-tagging-website.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['ptt-beauty-tagging-website.herokuapp.com']
 
 
 # Application definition
