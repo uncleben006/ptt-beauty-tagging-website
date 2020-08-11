be0cc884a89d22a3dbd129e8cbca95072a6ee553
ben_wang <uncleben006@gmail.com>
2020-08-11 20:24:37 +0800

    Update heroku config

--- ptt_beauty_django/settings.py
+++ ptt_beauty_django/settings.py
@@ -15,7 +15,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = 'https://ptt-beauty-tagging-website.herokuapp.com/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
