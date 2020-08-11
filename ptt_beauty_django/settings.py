5f40fab434453de8354ef133337f036fda9ed592
ben_wang <uncleben006@gmail.com>
2020-08-11 20:06:40 +0800

    Update heroku config

--- ptt_beauty_django/settings.py
+++ ptt_beauty_django/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ptt-beauty-tagging-website.herokuapp.com']
+ALLOWED_HOSTS = ['ptt-beauty-tagging-website.herokuapp.com','localhost']
 
 
 # Application definition
@@ -128,10 +128,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = '/static/'
