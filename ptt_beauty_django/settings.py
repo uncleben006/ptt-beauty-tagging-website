5f6c732a2448d8ce9a1f40cc02ccf79e4bfe07a7
ben_wang <uncleben006@gmail.com>
2020-08-11 19:33:52 +0800

    Update heroku config

--- ptt_beauty_django/settings.py
+++ ptt_beauty_django/settings.py
@@ -128,8 +128,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_URL = 'https://ptt-beauty-tagging-website.herokuapp.com/static/'
