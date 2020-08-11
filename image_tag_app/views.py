1a82ff2a418c5c620c944d0f705ac16ba25be133
ben_wang <uncleben006@gmail.com>
2020-08-11 17:04:13 +0800

    Fix the duplicate tags

--- image_tag_app/views.py
+++ image_tag_app/views.py
@@ -13,13 +13,14 @@ def index(request):
     page_obj = paginator.get_page(page_number)
 
     # 把 tag 轉成 distinct
+    tags_list = []
     if tags is not None:
         for tag in tags:
             if tag['tags'] is not None:
-                tag['tags'] = list(set(tag['tags']))
+                tags_list.extend(tag['tags'])
+    tags_list = list(set(tags_list))
 
-
-    return render(request, 'app/index.html', {'datas': datas, 'tags': tags, 'page_obj': page_obj})
+    return render(request, 'app/index.html', {'tags': tags_list, 'page_obj': page_obj})
 
 
 def post(request, slug):
@@ -49,12 +50,14 @@ def post(request, slug):
         tags = Data.objects.filter().values('tags')
 
         # 把 tag 轉成 distinct
+        tags_list = []
         if tags is not None:
             for tag in tags:
                 if tag['tags'] is not None:
-                    tag['tags'] = list(set(tag['tags']))
+                    tags_list.extend(tag['tags'])
+        tags_list = list(set(tags_list))
 
-        return render(request, 'app/post.html', {'datas': datas, 'tags': tags, 'slug': slug })
+        return render(request, 'app/post.html', {'datas': datas, 'tags': tags_list, 'slug': slug })
 
 def delete_img(request, slug, img):
     imgs = Data.objects.filter(slug__contains = slug).values('imgs')[0]['imgs']
